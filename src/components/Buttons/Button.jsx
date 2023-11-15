befd8c9ad3a43bafa49586220a76d131d3e39af7
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-11-15 15:04:59 -0300

    Instale bootstrap y fontawesome

--- src/components/Buttons/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = ({ children, classname, icon = "" }) => {
-    return (
-        <button className={ classname }><i className={ icon }></i> { children }</button>
-    );
-}
-
-export default Button;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <h1>Probar su codigo dentro de pages/Home.jsx</h1>
+    );
+}
+
+export default Home;
